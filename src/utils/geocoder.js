fd44965bbee94e7f7c097779a0cfe8c3b4609e60
Paulo Lima <paulobruny@gmail.com>
2020-06-30 22:16:20 -0300

    add and config geocoder with mapquest provider

---
+++ src/utils/geocoder.js
@@ -0,0 +1,12 @@
+import Geocoder from 'node-geocoder'
+
+const options = {
+  provider = process.env.GEOCODER_PROVIDER,
+  httpAdapter: 'https',
+  apiKey: process.env.GEOCODER_API_KEY,
+  formatter: null,
+}
+
+const geocoder = Geocoder(options)
+
+export default geocoder
