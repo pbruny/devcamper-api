d4b4415a7221e03db53f50366b2e3561016f2cb3
Paulo Lima <paulobruny@gmail.com>
2020-06-30 22:34:11 -0300

    fix typo setting provider

--- src/utils/geocoder.js
+++ src/utils/geocoder.js
@@ -1,7 +1,7 @@
 import Geocoder from 'node-geocoder'
 
 const options = {
-  provider = process.env.GEOCODER_PROVIDER,
+  provider: process.env.GEOCODER_PROVIDER,
   httpAdapter: 'https',
   apiKey: process.env.GEOCODER_API_KEY,
   formatter: null,
