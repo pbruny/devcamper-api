2ab6e0287734fd4581648e711b87afbb72737521
Paulo Lima <paulobruny@gmail.com>
2020-07-07 16:29:53 -0300

    add bootcamp relationship

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,19 @@
 import { Router } from 'express'
 import BootcampController from './controllers/BootcampController'
+import CourseController from './controllers/CourseController'
 
 
 const routes = new Router()
 
 routes.get('/api/v1/bootcamps', BootcampController.index)
 routes.get('/api/v1/bootcamps/:id', BootcampController.show)
+routes.get('/api/v1/bootcamps/:bootcampId/courses', CourseController.show)
 routes.get('/api/v1/bootcamps/radius/:zipcode/:distance', BootcampController.showBootcampsInRadius)
 routes.post('/api/v1/bootcamps', BootcampController.store)
 routes.put('/api/v1/bootcamps/:id', BootcampController.update)
 routes.delete('/api/v1/bootcamps/:id', BootcampController.delete)
+
+routes.get('/api/v1/courses', CourseController.index)
+routes.get('/api/v1/courses/:bootcampId', CourseController.show)
 
 export default routes
