3396ca01c3b857601d995793fd8a6322a661a593
Paulo Lima <paulobruny@gmail.com>
2020-07-09 17:33:32 -0300

    add jwt, bcrypt and User model

---
+++ src/models/User.js
@@ -0,0 +1,33 @@
+import Mongoose from 'mongoose'
+
+const UserSchema = new Mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please add a name']
+  },
+  name: {
+    type: String,
+    required: [true, 'Please add an email'],
+    unique: true,
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please add a valid email']
+  },
+  role: {
+    type: String,
+    enum: ['user', 'publisher'],
+    default: 'user'
+  },
+  password: {
+    type: String,
+    required: [true, 'Please add a password'],
+    minlength: 6,
+    select: false
+  },
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+export default Mongoose.model('User', UserSchema)
