199bdb2d18f8e6b6e7750c6c9a2ea36f582becbe
Paulo Lima <paulobruny@gmail.com>
2020-07-10 18:05:38 -0300

    handle photo upload ownership

--- src/controllers/FileController.js
+++ src/controllers/FileController.js
@@ -4,14 +4,20 @@ class FileController {
   async store(req, res) {
     const { filename: path } = req.file
 
-    const file = await Bootcamp.findByIdAndUpdate(req.params.id, {
-      photo: path
-    })
+    const file = await Bootcamp.findById(req.params.id)
 
     if(!file) {
       return res.status(404).json({success: false, error: `Bootcamp with id ${req.params.id} not found`})
     }
 
+    if(bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+      return res.status(400).json({success: false, error: `User with id of ${req.user.id} is not the owner of this bootcamp`})
+    }
+
+    const updateFile = await Bootcamp.findByIdAndUpdate(req.params.id, {
+      photo: path
+    })
+    
     return res.status(200).json({success: true, photo: path})
   }
 }
